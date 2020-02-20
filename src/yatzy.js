0841f70e08bb92572a3b3358c643bf90f787fe51
Viggo Petersen <viggo.petersen@sallinggroup.com>
2020-02-20 11:17:36 +0100

    Fix button container and move consts
    - move constants to variable file
    - add click event on dice
    - fix styling on selected dice

--- src/yatzy.js
+++ src/yatzy.js
@@ -3,3 +3,11 @@
 /**
  * Board logic
  */
+const selectDie = (event) => {
+    if (event.target.className == "eye") {
+        event.target.parentElement.classList.toggle("selected");
+        return;
+    }
+
+    event.target.classList.toggle("selected");
+} 
